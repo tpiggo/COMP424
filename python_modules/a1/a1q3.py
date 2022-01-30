ed0470141c03b8d1a2e2c709403933422c92a42f
tpiggo <tpiggo@gitlab.cs.mcgill.ca>
2022-01-30 00:42:55 -0500

    Hill climbing implemented and experiment completed!

--- python_modules/a1/a1q3.py
+++ python_modules/a1/a1q3.py
@@ -1,24 +1,25 @@
+import datetime
 import math
 import random
 import itertools
+import statistics as sts
 from typing import List
+import datetime as dt
 
 
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def gen_random(max_num):
+    i = int(random.random() * max_num)
+    i2 = int(random.random() * max_num)
+    while i == i2:
+        i2 = int(random.random() * max_num)
+    return i, i2
 
-    def __eq__(self, other):
-        return other.x == self.x and other.y == self.y
 
-    def __ne__(self, other):
-        return other.x != self.x and other.y != self.y
-
-    def distance_to_next(self, next):
-        x_distance = next.x - self.x
-        y_distance = next.y - self.y
-        return math.sqrt(math.pow(x_distance, 2) + math.pow(y_distance, 2))
+def check_in(list_of_items, element1, element2):
+    for x, y in list_of_items:
+        if (x == element1 and y == element2) or (x == element2 and y == element1):
+            return True
+    return False
 
 
 def recursive_permutations(element_to_perm, gen_perm, current_perm):
@@ -44,13 +45,36 @@ def find_random_path(elements, order):
     order.append(next_choice)
     return find_random_path(elements, order)
 
+
+def find_min_max_mean(input_list):
+    lmin = min(input_list)
+    lmax = max(input_list)
+    mean = sts.mean(input_list)
+    std = sts.stdev(input_list, mean)
+    return lmin, lmax, mean, std
+
+
+class City:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return other.x == self.x and other.y == self.y
+
+    def __ne__(self, other):
+        return other.x != self.x and other.y != self.y
+
+    def distance_to_next(self, next_city):
+        return math.sqrt(math.pow(next_city.y - self.y, 2) + math.pow(next_city.y - self.y, 2))
+
+
 class BaseInstance:
     def __init__(self):
         self.min = 0
         self.max = 0
         self.mean = 0
         self.std = 0
-        self.path_sum = 0
 
     def print_stats(self):
         print('For the following run we got:')
@@ -69,33 +93,37 @@ class TSPInstance(BaseInstance):
         self.random_path_length = 0
         self.random_path = None
         self.hill_climbed_path_length = 0
+        self._start_city = self._cities[0]
 
-    @staticmethod
-    def _calculate_tour(cities_order):
+    def get_num_cities(self):
+        return len(self._cities)
+
+    def _calculate_tour(self, cities_order):
+        if not self._start_city == cities_order[0]:
+            slice_index = 0
+            for index, city in enumerate(cities_order):
+                if city == self._start_city:
+                    slice_index = index
+                    break
+            front_order_slice = [cities_order[index] for index in range(slice_index, len(cities_order))]
+            rear_order_slide = [cities_order[index] for index in range(0, slice_index)]
+            cities_order = front_order_slice + rear_order_slide
         number_cities = len(cities_order)
         tour_distance = 0
         for index, city in enumerate(cities_order):
-            if number_cities == index + 1:
-                next_city = cities_order[0]
-            else:
-                next_city = cities_order[index + 1]
-            tour_distance += city.distance_to_next(next_city)
+            next_city = cities_order[(index + 1) % number_cities]
+            tour_distance = tour_distance + city.distance_to_next(next_city)
         return tour_distance
 
     def brute_force_solve(self):
         self.city_permutations = itertools.permutations(self._cities, len(self._cities))
         for cities_order in self.city_permutations:
-            self.tours.append(TSPInstance._calculate_tour(cities_order))
+            self.tours.append(self._calculate_tour(cities_order))
         self.calculate_reporting()
 
     def calculate_reporting(self):
-        self.max = self.tours[0]
-        self.min = self.tours[0]
-        for tour in self.tours:
-            if self.min > tour:
-                self.min = tour
-            elif self.max < tour:
-                self.max = tour
+        self.max = max(self.tours)
+        self.min = min(self.tours)
 
     def find_random_path(self):
         self.random_path = find_random_path(self._cities, [])
@@ -103,10 +131,9 @@ class TSPInstance(BaseInstance):
     def find_random_path_length(self):
         # No reporting here, only returning one
         self.find_random_path()
-        self.random_path_length = TSPInstance._calculate_tour(self.random_path)
+        self.random_path_length = self._calculate_tour(self.random_path)
 
     def solve_brute_with_random(self):
-        self.brute_force_solve()
         self.find_random_path_length()
 
     def _swap_city(self, swap_index1, swap_index2):
@@ -118,31 +145,61 @@ class TSPInstance(BaseInstance):
                 path.append(self.random_path[swap_index1])
             else:
                 path.append(city)
-        return path, len(path)
+        return path, self._calculate_tour(path)
+
+    def _compute_all_neighbours(self):
+        neighbours = []
+        num_cities = len(self.random_path)
+        for index in range(0, num_cities - 1):
+            for index1 in range(index + 1, num_cities):
+                neighbours.append(self._swap_city(index, index1))
+        return neighbours
+
+    def _compute_neighbour(self, computed):
+        max_combs = math.comb(len(self.random_path), 2)
+        if len(computed) == max_combs:
+            return None
+        max_length = len(self._cities)
+        i, i2 = gen_random(max_length)
+        while check_in(computed, i, i2):
+            i, i2 = gen_random(max_length)
+        computed.append((i, i2))
+        return self._swap_city(i, i2), computed
+
+    def solve_hill_climbing_for_7(self):
+        self.find_random_path_length()
+        current_tour_length = self.random_path_length
+        while True:
+            all_neighbours = self._compute_all_neighbours()
+            if len(all_neighbours) > 0:
+                path = all_neighbours[0][0]
+                min_distance = all_neighbours[0][1]
+                for neighbour in all_neighbours:
+                    if neighbour[1] < min_distance:
+                        min_distance = neighbour[1]
+                        path = neighbour[0]
+                if min_distance < current_tour_length:
+                    current_tour_length = min_distance
+                    self.random_path = path
+                else:
+                    break
+            else:
+                print("Failed to find all neighbours!")
+        self.hill_climbed_path_length = current_tour_length
 
     def solve_hill_climbing(self):
-        self.find_random_path()
-        current_tour_length = TSPInstance._calculate_tour(self.random_path)
-        # randomly choose a city
-        first_index = int(random.random() * len(self.random_path))
+        self.find_random_path_length()
+        current_tour_length = self.random_path_length
+        computed = []
         while True:
-            next_index = first_index + 1 if first_index < len(self.random_path) - 1 else 0
-            prev_index = first_index - 1 if first_index > 0 else len(self.random_path) - 1
-            if next_index == len(self.random_path):
-                print("GOING TO GO OUT OF BOUNDS!")
-            npath, nlength = self._swap_city(first_index, next_index)
-            ppath, plength = self._swap_city(first_index, prev_index)
-            if (plength == nlength or plength > nlength) and nlength < current_tour_length:
-                self.random_path = npath
-                current_tour_length = TSPInstance._calculate_tour(self.random_path)
-                first_index = next_index
-            elif plength < current_tour_length:
-                self.random_path = ppath
-                current_tour_length = TSPInstance._calculate_tour(self.random_path)
-                first_index = prev_index
-            else:
-                # Neither neighbour is optimal!
+            result = self._compute_neighbour(computed)
+            if result is None:
+                # Gone through all possible paths and found nothing better!
                 break
+            path_info, computed = result
+            if path_info[1] < current_tour_length:
+                self.random_path = path_info[0]
+                current_tour_length = path_info[1]
         self.hill_climbed_path_length = current_tour_length
 
     @staticmethod
@@ -158,7 +215,6 @@ class TSPInstance(BaseInstance):
             is_gen = False
             while not is_gen:
                 new_city = City(random.random(), random.random())
-                # is slow :(
                 is_gen = new_city not in cities
             cities.append(new_city)
         return TSPInstance(cities)
@@ -179,56 +235,19 @@ class AggregateTSPInstance(BaseInstance):
         self.hc_max = 0
         self.hc_std = 0
 
-    def _calculate_standard_deviation(self):
-        total = 0.0
-        for inst in self._tsp_instances:
-            total = total + math.pow(inst.min - self.mean, 2)
-        self.std = math.sqrt(total/len(self._tsp_instances))
-
     def _find_data_points(self):
-        path_sum = 0
-        for index, instance in enumerate(self._tsp_instances):
-            if index == 0:
-                self.max = instance.min
-                self.min = instance.min
-                path_sum = instance.min
-            elif self.min > instance.min:
-                self.min = instance.min
-            elif self.max < instance.min:
-                self.max = instance.min
-            path_sum += instance.min
-        self.mean = path_sum / len(self._tsp_instances)
-        # get std of optimal tour length
+        result_tuple = find_min_max_mean([tsp.min for tsp in self._tsp_instances])
+        self.min, self.max, self.mean, self.std = result_tuple
 
     def _find_data_points_rand(self):
-        path_sum = 0
-        for index, instance in enumerate(self._tsp_instances):
-            if index == 0:
-                self.r_max = instance.random_path_length
-                self.r_min = instance.random_path_length
-                path_sum = instance.random_path_length
-            elif self.r_min > instance.random_path_length:
-                self.r_min = instance.random_path_length
-            elif self.r_max < instance.random_path_length:
-                self.r_max = instance.random_path_length
-            path_sum += instance.random_path_length
-        self.r_mean = path_sum / len(self._tsp_instances)
+        result_tuple = find_min_max_mean([tsp.random_path_length for tsp in self._tsp_instances])
         # get std of optimal tour length
+        self.r_min, self.r_max, self.r_mean, self.r_std = result_tuple
 
     def _find_data_points_hc(self):
-        path_sum = 0
-        for index, instance in enumerate(self._tsp_instances):
-            if index == 0:
-                self.hc_max = instance.hill_climbed_path_length
-                self.hc_min = instance.hill_climbed_path_length
-                path_sum = instance.hill_climbed_path_length
-            elif self.hc_min > instance.hill_climbed_path_length:
-                self.hc_min = instance.hill_climbed_path_length
-            elif self.hc_max < instance.hill_climbed_path_length:
-                self.hc_max = instance.hill_climbed_path_length
-            path_sum += instance.hill_climbed_path_length
-        self.hc_mean = path_sum / len(self._tsp_instances)
+        result_tuple = find_min_max_mean([tsp.hill_climbed_path_length for tsp in self._tsp_instances])
         # get std of optimal tour length
+        self.hc_min, self.hc_max, self.hc_mean, self.hc_std = result_tuple
 
     def find_optimal_vs_rand(self):
         self.optimal_rand = 0
@@ -242,63 +261,58 @@ class AggregateTSPInstance(BaseInstance):
             if inst.min == inst.hill_climbed_path_length:
                 self.optimal_hc += 1
 
-    def _calculate_standard_deviation_hc(self):
-        total = 0.0
-        for inst in self._tsp_instances:
-            total = total + math.pow(inst.hill_climbed_path_length - self.hc_mean, 2)
-        self.hc_std = math.sqrt(total / len(self._tsp_instances))
-
-    def _calculate_standard_deviation_random(self):
-        total = 0.0
-        for inst in self._tsp_instances:
-            total = total + math.pow(inst.random_path_length - self.r_mean, 2)
-        self.r_std = math.sqrt(total/len(self._tsp_instances))
-
     def brute_force_all(self):
         if len(self._tsp_instances) == 0:
             raise ValueError("Missing instances to run this over!")
         for tsp_inst in self._tsp_instances:
             tsp_inst.brute_force_solve()
         self._find_data_points()
-        # get std of optimal tour length
-        self._calculate_standard_deviation()
 
-    def print_stats(self, print_random=False, print_hc=False):
-        super(AggregateTSPInstance, self).print_stats()
-        if print_random:
-            print('Random findings:')
-            print(f'Min: {self.r_min}')
-            print(f'Max: {self.r_max}')
-            print(f'Mean: {self.r_mean}')
-            print(f'Standard deviation: {self.r_std}')
-            print(f'Number of optimal: {self.optimal_rand}')
-        if print_hc:
-            print('Hill Climbing findings:')
-            print(f'Min: {self.hc_min}')
-            print(f'Max: {self.hc_max}')
-            print(f'Mean: {self.hc_mean}')
-            print(f'Standard deviation: {self.hc_std}')
+    def print_stats_rand(self):
+        print('Random findings:')
+        print(f'Min: {self.r_min}')
+        print(f'Max: {self.r_max}')
+        print(f'Mean: {self.r_mean}')
+        print(f'Standard deviation: {self.r_std}')
+        print(f'Number of optimal: {self.optimal_rand}')
+
+    def print_stats_hill_climbing(self, check_optimal=True):
+        print('Hill Climbing findings:')
+        print(f'Min: {self.hc_min}')
+        print(f'Max: {self.hc_max}')
+        print(f'Mean: {self.hc_mean}')
+        print(f'Standard deviation: {self.hc_std}')
+        if check_optimal:
             print(f'Number of optimal: {self.optimal_hc}')
 
     def random_paths(self):
-        self.path_sum = 0
         for tsp_inst in self._tsp_instances:
             tsp_inst.solve_brute_with_random()
         self._find_data_points()
-        self._calculate_standard_deviation()
         self._find_data_points_rand()
-        self._calculate_standard_deviation_random()
         self.find_optimal_vs_rand()
+        
+    def find_random_paths(self):
+        for tsp_inst in self._tsp_instances:
+            tsp_inst.find_random_path_length()
+        self._find_data_points_rand()
+
+    def solve_hill_climbing(self):
+        print(f"Starting hill climbing for {self._tsp_instances[0].get_num_cities()} instances!")
+        start = datetime.datetime.now()
+        for index, tsp_inst in enumerate(self._tsp_instances):
+            tsp_inst.solve_hill_climbing()
+            print('Done hill climbing for iteration {}'.format(index))
+        self._find_data_points_hc()
+        print("Done hill climbing")
+        print(f"Took {datetime.datetime.now() - start}")
 
     def brute_and_hillclimbing(self):
-        self.path_sum = 0
         for tsp_inst in self._tsp_instances:
-            tsp_inst.solve_hill_climbing()
             tsp_inst.brute_force_solve()
+            tsp_inst.solve_hill_climbing()
         self._find_data_points()
-        self._calculate_standard_deviation()
         self._find_data_points_hc()
-        self._calculate_standard_deviation_hc()
         self.find_optimal_vs_hill_climbing()
 
     @staticmethod
@@ -315,37 +329,39 @@ def run_brute_force(n=7):
     agg_tsp_instance.print_stats()
 
 
-def run_random_path(n=7):
+def run_random_vs_brute(n=7):
     agg_tsp_instance = AggregateTSPInstance.create_aggregate_tsp_instance(num_cities=n)
     agg_tsp_instance.random_paths()
-    agg_tsp_instance.print_stats(True)
+    agg_tsp_instance.print_stats()
+    agg_tsp_instance.print_stats_rand()
 
 
-def run_hill_climbing_single(n=7):
-    tsp = TSPInstance.generate_tsp_instance(n)
-    tsp.solve_hill_climbing()
-    tsp.brute_force_solve()
-    tsp.print_stats()
-    print(f"Length of the hill climbed path {tsp.hill_climbed_path_length}")
+def run_random(n=100):
+    agg_tsp_instance = AggregateTSPInstance.create_aggregate_tsp_instance(num_cities=n)
+    agg_tsp_instance.random_paths()
+    agg_tsp_instance.print_stats_rand()
 
-def run_hill_climbing(n=7):
+
+def run_hill_climbing_vs_brute(n=7):
     agg_tsp_instance = AggregateTSPInstance.create_aggregate_tsp_instance(num_cities=n)
     agg_tsp_instance.brute_and_hillclimbing()
-    agg_tsp_instance.print_stats(print_hc=True)
+    agg_tsp_instance.print_stats()
+    agg_tsp_instance.print_stats_hill_climbing()
+
+
+def run_hill_climbing(n=100):
+    agg_tsp_instance = AggregateTSPInstance.create_aggregate_tsp_instance(num_cities=n)
+    agg_tsp_instance.solve_hill_climbing()
+    agg_tsp_instance.print_stats_hill_climbing(False)
+
 
 def main():
     # run_brute_force()
     # run_random_path()
-    # run_hill_climbing_single()
-    run_hill_climbing()
-
-
-def test():
-    tsp_inst = TSPInstance.generate_tsp_instance(4)
-    distance = tsp_inst.find_random_path()
-    print(f'random path distance is {distance}')
+    run_random()
+    # run_hill_climbing()
+    # run_hill_climbing()
 
 
 if __name__ == '__main__':
     main()
-    # test()
